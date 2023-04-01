d545ddded04ce2c61b3204127c7f3914fc802994
all123all <allyson2308h@gmail.com>
2023-04-01 08:27:51 +0100

    feat: add first scripts

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='cirno-db',
+    version='0.1',
+    description='CirnoDB',
+    author='Henrique Marques',
+    author_email='allyson2308h@gmail.com',
+    url='https://github.com/all123all/cirno-db',
+    packages=['cirno'],
+)
